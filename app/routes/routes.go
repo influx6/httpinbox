9e2f14dda4be7583a9aa165adceab8c0e3709d16
Alexander Ewetumo <trinoxf@gmail.com>
2016-05-22 19:45:03 +0100

    first commit

---
+++ app/routes/routes.go
@@ -0,0 +1,8 @@
+package routes
+
+import "github.com/influx6/faux/web/app"
+
+// InitRoutes registers the routes for this app with the root runner.
+func InitRoutes(root *app.App) {
+
+}
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/ardanlabs/kit/cfg"
+	"github.com/influx6/faux/web/app"
+	"github.com/influx6/faux/web/middleware"
+	"github.com/influx6/httpinbox/app/routes"
+)
+
+func main() {
+
+	// Set the base level headers all response must contain.
+	baseHeaders := map[string]string{"X-App": "HttpInbox"}
+
+	// Make a new mongodb session middleware to be used globally.
+	dbm := middleware.MongoDBEnv("HTTPINBOX", nil)
+
+	// Create the server http.Handler conforming instance.
+	inbox := app.New(nil, true, baseHeaders, middleware.Log, dbm)
+
+	// Register all the routes.
+	routes.InitRoutes(inbox)
+
+	// Initialize the configuration system to retrieve environment variavles.
+	cfg.Init(cfg.EnvProvider{Namespace: "HTTPINBOX"})
+
+	// Retrieve the address we wish to use for the app.
+	addr := cfg.MustString("HOST_ADDR")
+
+	go func() {
+		http.ListenAndServe(addr, inbox)
+	}()
+
+	// Listen for an interrupt signal from the OS.
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+}
