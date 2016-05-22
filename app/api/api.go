c8c115ce9a1e24ee6713118e2a98c739416dff1d
Alexander Ewetumo <trinoxf@gmail.com>
2016-05-22 20:32:22 +0100

    updating vendoring files

---
+++ app/api/api.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"github.com/influx6/faux/context"
+	"github.com/influx6/faux/web/app"
+)
+
+// API defines an handle through which the HttpInbox API can be retrieved from.
+var API api
+
+// api defines a struct which holds all controller methods for the HttpInbox API.
+type api struct{}
+
+// NewInbox handles the creation of a new inbox for the reception of http requests.
+func (api) NewInbox(ctx context.Context, wq *app.ResponseRequest) error {
+
+	return nil
+}
--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,8 +1,11 @@
 package routes
 
-import "github.com/influx6/faux/web/app"
+import (
+	"github.com/influx6/faux/web/app"
+	"github.com/influx6/httpinbox/app/api"
+)
 
 // InitRoutes registers the routes for this app with the root runner.
 func InitRoutes(root *app.App) {
-
+	app.PageRoute(root, "GET", "/new", api.API.NewInbox)
 }
